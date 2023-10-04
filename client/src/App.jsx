efd8263301430cca828e8adfe4b80a396091fe20
aheeshnagraj <aheesh@gmail.com>
2023-10-03 19:02:06 -0700

    readme updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,14 +6,17 @@ import { useState } from "react";
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
-  const [privateKey, setPrivateKey] = useState("");
+  //const [privateKey, setPrivateKey] = useState("");
+  const [sign, setSign] = useState("");
 
   return (
     <div className="app">
       <Wallet
         balance={balance}
-        privateKey={privateKey}
-        setPrivateKey={setPrivateKey}
+        //privateKey={privateKey}
+        sign={sign}
+        //setPrivateKey={setPrivateKey}
+        setSign={setSign}
         setBalance={setBalance}
         address={address}
         setAddress={setAddress}
